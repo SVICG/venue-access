014a9a5e9f08a30caa9a6c4056403cfa3b2f43e8
Steph <thecrypttrip@gmail.com>
2026-04-15 15:53:37 +0100

    Added buffer areas
    Created buffers around venues and added to folium map

--- venue-access/src/mapping.py
+++ venue-access/src/mapping.py
@@ -4,6 +4,7 @@ import pandas as pd
 import geopandas as gpd
 import folium
 
+
 #access venue shp file
 venues = gpd.read_file('output/Venue Addresses.shp')
 
@@ -17,4 +18,24 @@ venues.explore (
                 marker_type= 'marker',
                 popup=['Venue Name'],
                 legend=False)
+
+#Project to ITM CRS to create buffers/polygons
+venues_proj = venues.to_crs(epsg=2157)
+venue_buffer = venues_proj.buffer(10000)
+
+#create GeoDataFrame and convert back to CRS4326 for addition to map
+buffer_gdf = gpd.GeoDataFrame(geometry=venue_buffer, crs=2157)
+buffer_web = buffer_gdf.to_crs(epsg=4326)
+
+#prevent overlapping areas from being counted twice
+buffers_union = buffer_gdf.dissolve()
+
+buffer_web.explore(
+    m=m,
+    style_kwds={'color': 'red', 'fillOpacity': 0.3},
+    name='5km Buffers'
+)
+
+
+
 
